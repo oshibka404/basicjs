1ab1611a278084a777cb76e58d5366276bbf016e
unknown <192.168.0.10\Mail_IMAP$>
2013-11-13 22:29:29 +0400

    Arbeit

---
+++ Voronkov/script.js
@@ -0,0 +1,28 @@
+var
+	l = prompt("LOGIN"),
+	def_log = "admin",
+	def_pwd = "admin";
+	
+
+//alert(s);	
+if (l == def_log) {
+	var p = prompt("PWD");
+	if (p = def_pwd) {
+		alert("YES");
+	} else {alert("NO");}
+	
+} else {alert("NO");}
+
+//console.log(typeof(b));	
+/*	
+& и
+| или
+^ искл. или
+~ не
+typeof - возвращает тип переменной
+parseInt - извлечение числа из строки
+console.log(s);  - метод вывода в консоль
+alert(""); - информацонное окно
+prompt(""); - поле ввода переменной
+confirm(""); - окно ОК/Отмена
+*/
