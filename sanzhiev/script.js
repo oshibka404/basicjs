455fbd4c69e0bd15552dcb7a96b1981b5c780802
seaone <eugenesanzhiev@gmail.com>
2013-11-13 22:31:48 +0400

    fix basicjs

---
+++ sanzhiev/script.js
@@ -0,0 +1,11 @@
+﻿
+var login = prompt ("Введите логин");
+
+if (login=="login"){
+	var pswrd = prompt ("Введите пароль")
+		if (pswrd == "123")
+			alert("Доступ к суперсекретной информации открыт!")
+
+} else {
+	alert ("Иди вон, негодяй!")
+}
