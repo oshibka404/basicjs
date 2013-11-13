e90e75a444bd92eaefcd2caf0b32a59afb358914
NatalyaCvet <natalya.cvet@gmail.com>
2013-11-13 22:32:58 +0400

    added folder

---
+++ Cvetkova/script.js
@@ -0,0 +1,43 @@
+var log = 'Cvet',
+	par = '1234';
+var log1 = prompt('Введите log');
+if (log == log1){
+var par1 = prompt('Введите par');
+if  (par == par1){
+alert('secret');
+}
+else alert('отказ');
+}
+else alert('отказ');
+/*var age = prompt('ск вам лет?');
+if (age===18){
+alert ('вам 18');
+}*/
+
+/*var t = "34",
+	s = "5";
+alert (typeof(t));
+alert (typeof(null));
+alert("string".len);
+alert(parseInt(t)-parseInt(s));*/
+/*var st = prompt("как дела");
+alert("у тебя все"+s);
+var s = 'колбаса';
+	t = s-1,//нан
+	i=1/0,// инфинити
+	i2=-1/0;// - инфинити
+//console.log(s);
+s=4;
+alert(s);
+alert(t);
+alert(i);
+alert(i2);
+console.log(t>0);
+console.log(t<0);*/
+// null, undefind
+/*foo();
+function foo() {
+s = 'in func';
+}
+alert(s);*/
+
