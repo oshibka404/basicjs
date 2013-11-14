29929c54593650c74a2a225f18b6f8e3c61ba544
Andy <Alyssalee@yandex.ru>
2013-11-15 02:41:06 +0400

    c:/basicjs

--- drozdova/script.js
+++ drozdova/script.js
@@ -1,8 +1,13 @@
 ﻿var login="Mod";
 var password="qwerty123";
+var i = 0;
 var userlogin=prompt("Введите логин");
 if (login==userlogin) {
 var userpassword=prompt("Введите пароль")};
+while (userpassword!=password && i<4) {
+var userpassword=prompt("Введите пароль");
+i++;
+};
 if (password==userpassword) {
 alert("секретная инфа: логин " + login + ", пароль " + password);
 };
