4dff53f82f879cd819cc5c64a3b3296f02280052
Anndy <alyssalee@yandex.ru>
2013-11-15 04:15:08 +0400

    script.js

--- drozdova/script.js
+++ drozdova/script.js
@@ -2,9 +2,8 @@
 var password="qwerty123";
 var i = 0;
 var userlogin=prompt("Введите логин");
-if (login==userlogin) {
-var userpassword=prompt("Введите пароль")};
-while (userpassword!=password && i<4) {
+if (login==userlogin) {};
+while (userpassword!=password && i<5) {
 var userpassword=prompt("Введите пароль");
 i++;
 };
