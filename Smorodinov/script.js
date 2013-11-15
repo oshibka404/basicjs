dab711b8cbaf162d706c511e0587a0f7af190e51
SDS <smorodinov.denis@gmail.com>
2013-11-15 13:27:50 +0400

    Home work

--- Smorodinov/script.js
+++ Smorodinov/script.js
@@ -2,21 +2,29 @@
 autorization();
 
 function autorization(){
-	while (true) {
+	outer: while (true) {
 
 		var login = prompt("Введите логин:");
 		if (login == "login") {
-			var pass = prompt("Введите пароль:");
-			if (pass == "password") {
-				alert("Суперсекретная информация");
-				break;
-			}
-			else{
-				alert("Пароль неверен");
+			for (i = 0; i < 5; i++){
+				var pass = prompt("Введите пароль:");
+				if (pass == "password") {
+					alert("Суперсекретная информация");
+					break outer;
+				}
+				else{
+					if (i == 4){
+						alert("Вам отказано в авторизации");
+						break outer;
+					}
+					else{
+						alert("Пароль неверен, осталось попыток: " + (4 - i));
+					}
+				}
 			}
 		}
 		else{
-			alert("Логин неверен");
+			alert("Такой логин не зарегистрирован");
 		}
 
 	}
