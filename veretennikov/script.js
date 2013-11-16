43147f6c352074dbda17ee81d421cd08aa3e0a0e
pavelflyweb <pavel.flyweb@gmail.com>
2013-11-16 19:46:48 +0400

    Авторизация с циклами

--- veretennikov/script.js
+++ veretennikov/script.js
@@ -22,12 +22,21 @@
 else alert ('Неверный логин'); 
 ------------- авторизация без циклов ------------------	
 */
-var i=0;
-do {
-var login = prompt("Логин");
-} while (login!=='ключ');
-do {
-var pass = prompt("Пароль");
-i++;
-} while (pass!=='123');
-alert ('Секретная информация');
+var i;
+var n;
+for (i=0; i<5; i++)
+{
+	var login = prompt("Enter login");
+	if (login=='123') 
+	{
+		for (n=0; n<5; n++)
+		{
+			var pass = prompt("Enter password");
+			if (pass=='123') {alert ('Секретная информация'); break;}
+			if (n>=4) {alert ('Ошибка авторизации');}
+		}
+	}
+	if (i>=4) {alert ('Ошибка авторизации');}
+	if (pass=='321') break;
+	if (n>=4) break;
+}
