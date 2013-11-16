38cbd8def15fdd2a552ede78818c96d89e2bb351
unknown <192.168.0.10\Mail_IMAP$>
2013-11-16 19:42:37 +0400

    Задание 2

--- Voronkov/script.js
+++ Voronkov/script.js
@@ -1,24 +1,28 @@
-var
-	l = prompt("LOGIN"),
-	def_log = "admin",
-	def_pwd = "admin";
-	
+var	
+    default_login = "admin",
+    default_pwd = "admin",
+    entered_login = prompt("Введите имя пользователя");
 
-//alert(s);	
-if (l == def_log) {
-	var p = prompt("PWD");
-	if (p = def_pwd) {
-		alert("YES");
-	} else {alert("NO");}
-	
-} else {alert("NO");}
+if (entered_login == default_login) {
+	var entered_pwd = prompt("Введите пароль");
+        for(var i=4; i>0; i--) {
+            if (entered_pwd == default_pwd) {
+		alert("Ты таки взломал систему, мегахакер!");
+                break;
+            } else {entered_pwd = prompt("Неверный пароль. Попробуйте ещё раз. Осталось попыток - "+i);} 
+        }
+        
+        if (i==0) {
+            alert("Отказано в доступе!");
+        }
+} else {alert("Неверное имя пользователя");}
 
-//console.log(typeof(b));	
+
 /*	
-& и
-| или
-^ искл. или
-~ не
+& and
+| or
+^ xor
+~ no
 typeof - возвращает тип переменной
 parseInt - извлечение числа из строки
 console.log(s);  - метод вывода в консоль
@@ -26,3 +30,4 @@ alert(""); - информацонное окно
 prompt(""); - поле ввода переменной
 confirm(""); - окно ОК/Отмена
 */
+
