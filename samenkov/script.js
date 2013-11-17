65bd236cfd43c3b16cdb27a7c33bc9d33ec85b92
Dupleks <samenkov_egor@mail.ru>
2013-11-17 15:39:26 +0400

    Добавлено 5 попыток авторизации в script.js

--- samenkov/script.js
+++ samenkov/script.js
@@ -1,7 +1,24 @@
-var a = prompt("Login?")
-if (a == 123) {
-	var b = prompt('Password?')
+var a = prompt("Login?");
+if (a == "123")
+{
+	var i = 0;
+	while (i < 5)
+	{
+		var b = prompt('Password?');
+  		if (b == "123") break;
+		i++;
+		alert("Invalid Password. Try again")
+	}
+if (b == "123") 
+{
+alert('All right')
 }
-if (b == 123){
-	alert('все хорошо')
+else 
+{
+	alert('Ошибка авторизации')
+}
+}
+else 
+{
+	alert('Ошибка авторизации')
 }
