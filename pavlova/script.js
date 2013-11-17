0eb2513465afc9bfa1d772efa69416543e12f6c2
chwyrligwgan <chwyrligwgan@qmail.com>
2013-11-17 19:09:54 +0400

    add messages massive

--- pavlova/script.js
+++ pavlova/script.js
@@ -1,28 +1,28 @@
 function avtorization() {
 	var login,
-		logins = ["manager", "developer", "user"],
+		logins = ['manager', 'developer', 'user'],
 		password,
 		passwords = ['1', '12', '123'],
-		count = 0;
+		index = 0,
+		count = 0,
+		messages = ['big secret', 'medium secret', 'small secret'];
 		
 	login = prompt("login:");
-	
-	if (logins.indexOf(login) != -1) {
-		while (password != passwords[logins.indexOf(login)] && ++count <= 5)
+	index = logins.indexOf(login);
+	if ( index != -1) {
+		while (password != passwords[index] && ++count <= 5)
 			password = prompt("password:");
 			
-		if (password == passwords[logins.indexOf(login)])
+		if (password == passwords[index])
 			switch (login)
 			{
 				case "manager":
-					alert("big secret");
-					break;
 				case "developer":
-					alert("medium secret");
-					break;
 				case "user":
-					alert("small secret");
+					alert(messages[index]);
 					break;
+				default:
+					alert("You are outsider!");
 			}
 		else
 			alert("!ERROR: password incorrect");
