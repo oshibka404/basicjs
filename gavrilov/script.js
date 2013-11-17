932e3a68f8db036f9617b610912cffe3d195cc50
bitaruka <bitaruka@gmail.com>
2013-11-17 17:56:39 +0400

    ultra functionality

--- gavrilov/script.js
+++ gavrilov/script.js
@@ -1,15 +1,43 @@
 var login = prompt("Введите логин:")
 var inv = 0
-if (login == "login") {
+switch (login) {
+	case "Виталька": 
 	verification:
 	for (inv; inv<5; inv++) {
 		var password = prompt ("Введите пароль ")
-		if (password == "password") {
-			alert ("Our secret");
+		if (password == "vitaliwe") {
+			alert ("Your secret");
 			break 
 		} else continue verification;		
 	} 
 	if (inv==5) {
 		alert("Access denied")
-	}
-} else alert("Access denied")
+	} break
+	case "Админ": 
+	verification:
+	for (inv; inv<5; inv++) {
+		var password = prompt ("Введите пароль ")
+		if (password == "adminiwe") {
+			alert ("ОМГ");
+			break 
+		} else continue verification;		
+	} 
+	if (inv==5) {
+		alert("Access denied")
+	} break
+	case "Лалка": 
+	verification:
+	for (inv; inv<5; inv++) {
+		var password = prompt ("Введите пароль ")
+		if (password == "dniwe") {
+			alert ("Access denied");
+			alert ("Затралел)))) Так задумано, лалка");
+			break 
+		} else continue verification;		
+	} 
+	if (inv==5) {
+		alert("Access denied")
+	} break
+ 	default:
+ 		alert("Access denied")
+ }
