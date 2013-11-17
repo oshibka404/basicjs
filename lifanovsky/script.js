73838a462b4b3227ef6a6d478ad6c103324a88c0
vlif <vlif.developer@gmail.com>
2013-11-17 17:52:06 +0400

    switch

--- lifanovsky/script.js
+++ lifanovsky/script.js
@@ -2,17 +2,24 @@ var login = prompt("Введите логин");
 if(login == "spiderman") {
     for(var i=0; i<5; i++) {
         var pass = prompt("Введите пароль");
-        if(pass == "asdzxc") {
-            alert("Даешь супергерою секретную инфу!");
+        var check = false;
+        switch(pass) {
+            case "asdzxc":
+                alert("Даешь супергерою секретную инфу!");
+                check = true;
             break;
-        } else {
-            if(4-i==0) {
-                alert("Попыток больше нет :(");
-            } else {
-                alert("Количество попыток: "+(4-i));
-            }
-            
+            case "123":
+                alert("Тоже даешь супергерою секретную инфу!");
+                check = true;
+            break;
+            default:
+                if(4-i==0) {
+                    alert("Попыток больше нет :(");
+                } else {
+                    alert("Количество попыток: "+(4-i));
+                }
         }
+        if(check) break;
     }
 } else {
     alert("Нет доступа!");
