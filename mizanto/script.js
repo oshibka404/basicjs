7960f9bdd0f29fd58b8462e3c3994e86d41eef3d
Sergey Bendak <mizanto@MacBook-Pro-Sergey.local>
2013-11-17 17:57:30 +0400

    3 users added

--- mizanto/script.js
+++ mizanto/script.js
@@ -19,19 +19,38 @@ if (age < 18) {
 */
 
 
-valid_login = "user";
+valid_user_login = "user";
+valid_admin_login = "admin";
+valid_guest_login = "guest";
+
 valid_password = "password";
 secure_message = "СУПЕРСЕКРЕТНАЯИНФОРМАЦИЯ";
 
 var login = prompt("Введите имя пользователя: ");
 
-if (login == valid_login) {
+switch (login) {
+
+	case valid_user_login:
+	passvordValidation();
+	break;
+
+	case valid_admin_login:
+	passvordValidation();
+	break;
+
+	case valid_guest_login:
+	passvordValidation();
+	break;
+
+	default:
+	alert("Вы ввели неверное имя пользователя.");
+}
+
+function passvordValidation() {
 	var password = prompt("Введите пароль: ");
 	if (password == valid_password) {
 		alert(secure_message);
 	} else {
 		alert("Вы ввели неверный пароль.");
 	}
-} else {
-	alert("Вы ввели неверное имя пользователя.");
 }
