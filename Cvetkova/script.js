bdfe10529d81b6dc200aee5ec6c76f8a8d126fed
NatalyaCvet <natalya.cvet@gmail.com>
2013-11-17 17:55:29 +0400

    switch

--- Cvetkova/script.js
+++ Cvetkova/script.js
@@ -2,6 +2,15 @@
 	par = '1234',
 	n = 5;
 var log1 = prompt('Введите логин.');
+switch (log1) {
+	case 'Cvet': alert ('Пользователь '+ log1);
+	break;
+	case 'Test': alert ('Пользователь '+ log1);
+	break;
+	case 'Test1': alert ('Пользователь '+ log1);
+	break;
+	default: alert ('Неопознан');
+	}
 if (log == log1){
 	var i=1;
 	while (i<=n){
