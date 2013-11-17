b9b5e1e4396854fb0020b76e7a23cd55138519dc
seaone <eugenesanzhiev@gmail.com>
2013-11-17 17:59:52 +0400

    17112013

--- sanzhiev/script.js
+++ sanzhiev/script.js
@@ -1,4 +1,5 @@
 ﻿
+/*
 var login = prompt ("Введите логин");
 
 if (login=="login"){
@@ -9,3 +10,42 @@ if (login=="login"){
 } else {
 	alert ("Иди вон, негодяй!")
 }
+*/
+
+
+var login = prompt ("Введите логин!");
+
+	switch (login){
+	case "Eugene":
+		alert("Привет! Теперь введи пароль!");
+	break;
+	case "Nastya":
+		alert("До свидания!");
+	break;
+	default:
+		alert("Пока!");
+}
+
+if (login=="Eugene"){
+	for(var i=1; i < 6; i++){
+	var pass = prompt("Введите пароль");
+	if (pass == "1234"){
+		alert("Доступ к суперсекретной информации открыт!");
+		break;
+	}
+	else{
+		if(5-i==0){
+			alert("Попытки исчерпаны");
+		}
+		else{
+			alert("Количество попыток: "+(5-i));
+		}
+	}
+	}
+}
+else{
+	alert("Нет доступа!");
+}
+  
+  
+
