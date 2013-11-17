ae734f89b72aebe9da7a6369f8c4e23a04e43720
Pavel Murkin <kot_tdf@mail.ru>
2013-11-17 17:57:15 +0400

    switch/case

--- Pavel_Pustota/script.js
+++ Pavel_Pustota/script.js
@@ -1,13 +1,39 @@
 ﻿for (i=0; i<5; i++) {
 	var login = prompt('Введи логин, у тебя осталось '+(5-i)+' попыток');
-	if (login==='demo'){
-		for (i=0; i<5; i++) {
-			var password = prompt('Введи пароль, у тебя осталось '+(5-i)+' попыток');
-			if (password==='demo'){
-				console.log('You win');
-				break;
-		  	};
-		};	
-		break;
+	var s=false;
+	switch(login){
+		case "Paul":
+			for (y=0; y<5; y++) {
+				var password = prompt(login+' введи пароль, у тебя осталось '+(5-y)+' попыток');
+				if (password==='demo'){
+					console.log('You win '+login);
+					s=true;
+					break;
+			  	};
+			};	
+			break;
+		case "Paul2":
+			for (y=0; y<5; y++) {
+				var password = prompt(login+' веди пароль, у тебя осталось '+(5-y)+' попыток');
+				if (password==='demo'){
+					console.log('You win '+login);
+					s=true;
+					break;
+			  	};
+			};	
+			break;
+		case "Paul3":
+			for (y=0; y<5; y++) {
+				var password = prompt(login+' веди пароль, у тебя осталось '+(5-y)+' попыток');
+				if (password==='demo'){
+					console.log('You win '+login);
+					s=true;
+					break;
+			  	};
+			};	
+			break;
+		 default:	
+			break;
 	};
- };
+	if(i==5||s)break;
+};
