45826be1bd70c30258f3a1f96210c9ba5ca4cf9c
Anndy <alyssalee@yandex.ru>
2013-11-17 18:06:10 +0400

    script.js

--- drozdova/script.js
+++ drozdova/script.js
@@ -1,17 +1,55 @@
-var login="Mod";
+var login1="Nastya";
+var login2="Katya";
+var login3="Natasha"
+var login = prompt("Ввведите имя")
 var password="qwerty123";
-var i = 0;
-var userlogin=prompt("Введите логин");
-if (login==userlogin) {;
-	while (userpassword!=password && i<5) {
-		var userpassword=prompt("Введите пароль");
-		i++;
-		};
-		if (password==userpassword) {
-			alert("секретная инфа: логин " + login + ", пароль " + password);
-		}
-		else { alert("Неверный пароль");
-		}			
-}
-else { alert("Неверный логин");
-}
+var i=0;
+switch (login) {
+	case login1:
+	alert("Логин хороший");
+		break
+	case login2:
+		alert("Логин пойдет");
+		break
+	case login3:
+		alert("Логин не очень, но так и быть");
+		break
+	default:
+		alert("логин неверен");
+		break
+};
+if (login==login1) {
+        while (userpassword!=password && i<5) {
+                var userpassword=prompt("Введите пароль");
+                i++;
+                };
+                if (password==userpassword) {
+                        alert("секретная инфа: логин " + login + ", пароль " + password);
+                }
+                else { alert("Неверный пароль");
+                }                        
+};
+if (login==login2) {
+        while (userpassword!=password && i<5) {
+                var userpassword=prompt("Введите пароль");
+                i++;
+                };
+                if (password==userpassword) {
+                        alert("секретная инфа: логин " + login + ", пароль " + password);
+                }
+                else { alert("Неверный пароль");
+                }                        
+};
+if (login==login3) {
+        while (userpassword!=password && i<5) {
+                var userpassword=prompt("Введите пароль");
+                i++;
+                };
+                if (password==userpassword) {
+                        alert("секретная инфа: логин " + login + ", пароль " + password);
+                }
+                else { alert("Неверный пароль");
+                }                        
+};
+
+	
