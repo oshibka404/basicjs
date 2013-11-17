6608ebfc8eaafa9d8bc998832e4d73c209347d0b
chwyrligwgan <chwyrligwgan@qmail.com>
2013-11-17 18:49:21 +0400

    add massive

--- pavlova/script.js
+++ pavlova/script.js
@@ -1,60 +1,17 @@
-/*
-g = "global";
-var str = "str",	// local
-	
-	n = str - 1,	// NAN != itself, 0, infinity - strong tipization
-	
-	iP = 1/0,		// infinity
-	iM = -iP,
-	
-	nan = iP - iP;	// iP +/- iM
-
-var somevar;		// undefined: not contain nothing
-var nullvar = null;	// contain nothing
-
-alert(str);
-alert(n);
-alert(nan);
-alert(iP);
-alert(iM);
-alert(somevar);
-alert(nullvar);
-
-console.log(n==0);
-console.log(n==n);
-
-var s = prompt("how are you?");
-alert("you are " + s);
-
-var s = "100";
-var s2 = 5;
-
-alert(parseInt(s) + 5);
-alert(s + s2);
-
-alert(typeof null);
-alert((100).toString().length);
-
-var age = prompt("How old are you?");
-alert(typeof age);
-if (age < 18)
-	alert("you'll not pass");
-else if(age === "18")
-	alert("you are 18");
-*/
-
 function avtorization() {
 	var login,
+		logins = ["manager", "developer", "user"],
 		password,
+		passwords = ['1', '12', '123'],
 		count = 0;
 		
 	login = prompt("login:");
 	
-	if (login == "manager" || login == "developer" || login == "user") {
-		while (password != "12345" && ++count <= 5)
+	if (logins.indexOf(login) != -1) {
+		while (password != passwords[logins.indexOf(login)] && ++count <= 5)
 			password = prompt("password:");
 			
-		if (password == "12345")
+		if (password == passwords[logins.indexOf(login)])
 			switch (login)
 			{
 				case "manager":
