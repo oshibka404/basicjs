85d40f5821bd0c91c8481de6f902bda1e795bf82
vlif <vlif.developer@gmail.com>
2013-11-20 00:30:56 +0400

    recursion

--- lifanovsky/script.js
+++ lifanovsky/script.js
@@ -1,4 +1,4 @@
-var login = prompt("Введите логин");
+/*var login = prompt("Введите логин");
 if(login == "spiderman") {
     metka:for(var i=0; i<5; i++) {
         var pass = prompt("Введите пароль");
@@ -19,4 +19,25 @@ if(login == "spiderman") {
     }
 } else {
     alert("Нет доступа!");
+}*/
+
+var sample = [100, "Они близко", 300, "Он тот, кто ждет за стеной", [1,2,3,4],
+            500, 600, [10, 20, [-1, -2, [-10, -20, "Прячься"], -3, -4], 30,
+            "Скоро будет поздно"], 700, 800, "Ничто не спасет"];
+
+var result = "";
+var foo = function(arr) {
+    //Эту функцию надо наполнить кодом, а вне её ничего не трогать.
+    //alert(arr.length);
+    for(var i=0; i<arr.length; i++){
+        if(typeof(arr[i]) == "object"){
+            foo(arr[i]);
+        } else {
+            //alert(arr[i]);
+            result += arr[i].toString()+" ";
+        }
+    }
+    return result;
 }
+
+alert(foo(sample));
