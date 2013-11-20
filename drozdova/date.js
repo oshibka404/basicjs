36730f34365d73c1385dde5b6458b30ddf2256b5
Anndy <alyssalee@yandex.ru>
2013-11-20 21:21:13 +0400

    date.js

--- drozdova/date.js
+++ drozdova/date.js
@@ -1,7 +1,10 @@
-var cur = new Date();
+function sec10() 
+{var cur = new Date();
 var a = cur.getHours();
 var b = cur.getMinutes();
 var c = cur.getDate();
 var d = cur.getMonth();
 var e = cur.getFullYear();
 alert(a + ":" + b + " " + c + "." + d + " " + e + " года");
+};
+setInterval(sec10,10000);
