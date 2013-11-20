19d1f78c2b7e58170488d4c53a8c57b6333f954a
Basov Leonid <basov.leonid@gmail.com>
2013-11-20 21:19:58 +0400

    current Date print with timeout

--- lbasov/date.js
+++ lbasov/date.js
@@ -3,9 +3,17 @@
  */
 
 // 00:00 01.03 2119 года
-function printDate() {
-    var d = new Date();
+function getDateString( d ) {
     var dateString = d.getHours() +":"+ d.getMinutes()+" ";
-    dateString += d.getDate()+"."+ d.getUTCMonth()+ " "+ d.getFullYear() + " года";
+    dateString += d.getDate()+"."+ d.getMonth()+ " "+ d.getFullYear() + " года";
     return dateString;
+}
+
+
+function startPrintingDate(){
+    var printingFunction = function(){
+        alert(  getDateString( new Date() ) );
+    };
+    setInterval( printingFunction , 1000);
+
 }
