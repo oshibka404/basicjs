2ae3222feb044f50e5829c182ecbf0ebe92821ba
Pavel Murkin <kot_tdf@mail.ru>
2013-11-20 21:05:26 +0400

    setInterval

--- Pavel_Pustota/script.js
+++ Pavel_Pustota/script.js
@@ -1,3 +1,13 @@
-﻿var current = new Date();
-alert(current.getMinutes()+':'+current.getHours()+' '+current.getDay()+'.'+current.getMonth()+' '+current.getFullYear()+' года');
+﻿function go() {
+	var interval = setInterval(sayHi, 10000);
+}
+function go2() {
+	var timeout = setTimeout(sayHi, 2000);
+}
 
+function sayHi() {
+	var current = new Date();
+	alert(current.getSeconds()+':'+current.getMinutes()+':'+current.getHours()+' '+current.getDay()+'.'+current.getMonth()+' '+current.getFullYear()+' года');
+}
+
+go();
