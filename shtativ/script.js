fccd594f88689b36a73682efa165177d937955aa
Kai <xiamkaix@gmail.com>
2013-11-20 13:40:14 +0300

    Update script.js

--- shtativ/script.js
+++ shtativ/script.js
@@ -4,7 +4,7 @@ var a,
 a = prompt ("Enter your username");
 if ( a == "root" ) {
     for ( c = 5; c > 0; c-- ) {
-        b = prompt("Enter your password. There are " + c + " trys");
+        b = prompt("Enter your password. There are " + c + " tries");
         
         if ( b == "pass" ) {
             alert ("=======secret info=========");
