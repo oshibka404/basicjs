192d380bb6983044d3f9f4b25e1f0539c22d9b89
SDS <smorodinov.denis@gmail.com>
2013-11-20 22:02:43 +0400

    change links

--- Smorodinov/20-11.js
+++ Smorodinov/20-11.js
@@ -1,9 +1,80 @@
-//00:00 01.03.2119 
+
+
+var cont = document.getElementById('container');
+var links = cont.getElementsByTagName('a');
+
+for (var i = 0; i < links.length; i++) {
+	links[i].href = "#";
+}
+
+var secCont = document.getElementById('secondContainer');
+
+secCont.innerHTML = "<a href='http://yahoo.com'>Yahoo!</a>";
+
+
+/*
+
+//Обращение к документу
+
+var links = document.getElementsByTagName('a');
+console.log(links);
+
+for (var i = 0; i < links.length; i++) {
+	links[i].href = "http://ya.ru";
+	links[i].style.fontWeight = "bold";
+	links[i].setAttribute("style","color: magenta; font-style: italic");
+}
+
+var para = document.getElementById('para');
+para.innerHTML += " <a href='#'>newText</a>";
+//para.innerText += "new text";
+
+
+//Функция-часы
+
+function goMad() {
+	var interval = setInterval(sayDate, 1000);
+}
+
+function sayDate (){
+	var current = new Date();
+	document.body.innerHTML= "";
+	document.write(current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds() + " " + current.getDate() + "." + current.getMonth() + " " + current.getFullYear() + " года");
+}
+
+goMad();
+
+
+
+//Анонимные функции
+timeout = setTimeout(function() {
+	var current = new Date();
+}, 10000);
+
+
+timeout = setTimeout(foo, 10000); - функция вызывается без скобок, т.к. в противном случае передаётся не сама функция, а её значение
+
+function foo() {}
+
+//Функция запускает алерт каждые 3 секунды
+
+function goMad() {
+	var interval = setInterval(sayHi, 3000);
+}
+
+function sayHi (){
+	alert("Привет!");
+}
+
+
+
+//00:00 01.03.2119 - вывод текущего времени
 
 var current = new Date();
 document.write(current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds() + " " + current.getDate() + "." + current.getMonth() + " " + current.getFullYear() + " года");
 
-/*
+
+//Конструктор робота
 
 function Robot(name) {
 	var x = 0,
